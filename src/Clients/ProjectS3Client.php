f2ad5e85bdc095a5eb65612da36dc5e6b7904aa0
Naldson Chagas <naldson.bc@gmail.com>
2023-09-29 17:19:14 -0300

    feat: Geting queue messages from sqs

---
+++ src/Clients/ProjectS3Client.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ReverseGeocode\ReverseGeocodeMicroservice\Clients;
+
+class S3Client implements StorageClient
+{
+
+    public function __construct()
+    {
+        
+    }
+
+    public function getFile($key)
+    {
+
+    }
+}
